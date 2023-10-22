fb952102f05c6362a5ab6b621b58a129f9bf014a
EliasOjuara <araujoelias616@gmail.com>
2023-10-22 08:05:27 -0300

    Alteração no test

--- src/test.js
+++ src/test.js
@@ -1,28 +1,28 @@
 function calcularNivel(vitorias, derrotas) {
-    const saldoVitorias = vitorias - derrotas;
-    let nivel = "";
+    const saldoVitorias = vitorias - derrotas
+    let nivel = ""
 
     if (vitorias < 10) {
-        nivel = "Ferro";
+        nivel = "Ferro"
     } else if (vitorias >= 11 && vitorias <= 20) {
-        nivel = "Bronze";
+        nivel = "Bronze"
     } else if (vitorias >= 21 && vitorias <= 50) {
-        nivel = "Prata";
+        nivel = "Prata"
     } else if (vitorias >= 51 && vitorias <= 80) {
-        nivel = "Ouro";
+        nivel = "Ouro"
     } else if (vitorias >= 81 && vitorias <= 90) {
-        nivel = "Diamante";
+        nivel = "Diamante"
     } else if (vitorias >= 91 && vitorias <= 100) {
-        nivel = "Lendário";
+        nivel = "Lendário"
     } else {
-        nivel = "Imortal";
+        nivel = "Imortal"
     }
 
-    return `O Herói tem saldo de ${saldoVitorias} e está no nível de ${nivel}`;
+    return `O Herói tem saldo de ${saldoVitorias} e está no nível de ${nivel}`
 }
 
 // Exemplo de uso:
-const vitorias = 85;
-const derrotas = 15;
-const resultado = calcularNivel(vitorias, derrotas);
-console.log(resultado);
+const vitorias = 85
+const derrotas = 15
+const resultado = calcularNivel(vitorias, derrotas)
+console.log(resultado)
